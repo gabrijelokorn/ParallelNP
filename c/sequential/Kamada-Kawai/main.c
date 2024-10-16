a1adb049dd5bf312de701a263211b81208151432
gabrijelokorn <gabrijel.okorn@gmail.com>
2024-10-16 18:58:57 +0200

    fix: instructions

--- c/sequential/Kamada-Kawai/main.c
+++ c/sequential/Kamada-Kawai/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     // At least two arguments expected - input file name and output file name
     if (argc < 5)
     {
-        printf("[%s:] Internal error - try: %s <points.txt> <coordinates.txt> <edges.txt> <output video.avi>\n", NAME, argv[0]);
+        printf("[%s:] Internal error - try: %s <points.txt> <coordinates.txt> <edges.txt> \n", NAME, argv[0]);
         return 1;
     }
 
