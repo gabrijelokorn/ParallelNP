9bd9efcd7d003e25baed9cb92b161c44967bc3ba
x <gabrijel.okorn@frida>
2025-04-07 23:07:02 +0000

    supercomputer synch

--- src/c/partition/algo/algo.c
+++ src/c/partition/algo/algo.c
@@ -30,10 +30,10 @@ bool *run_algo(Partitions *p, void (*func)(Partitions *, bool *), char *name, ch
 void algo(Partitions *p, char *test_id)
 {
     run_algo(p, seq, "seq", test_id);
+    run_algo(p, sgl_stc, "sgl_stc", test_id);
+    run_algo(p, sgl_dyn, "sgl_dyn", test_id);
     run_algo(p, mlt_stc, "mlt_stc", test_id);
     run_algo(p, mlt_dyn, "mlt_dyn", test_id);
-    run_algo(p, sgl_dyn, "sgl_dyn", test_id);
-    run_algo(p, sgl_stc, "sgl_stc", test_id);
     run_algo(p, nested, "nested", test_id);
 
     return;
