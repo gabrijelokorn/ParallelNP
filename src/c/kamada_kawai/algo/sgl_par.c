6eb6424b4de932f8381d71b51d1bee37dd7e0aa1
gabrijelokorn <gabrijel.okorn@gmail.com>
2025-08-22 16:25:13 +0200

    .

--- src/c/kamada_kawai/algo/sgl_par.c
+++ src/c/kamada_kawai/algo/sgl_par.c
@@ -13,19 +13,19 @@ void sgl_par(KamadaKawai *kk)
         while (kk->deltas[delta_max_index] > kk->epsilon)
         {
             double d_x_m = 0, d_y_m = 0, d_xx_m = 0, d_yy_m = 0, d_xy_m = 0;
-            get_derivatives_seq(kk, delta_max_index, &d_x_m, &d_y_m, &d_xx_m, &d_yy_m, &d_xy_m); // at first seeable speedup, then progressively worse overhead with more threads
+            get_derivatives_par(kk, delta_max_index, &d_x_m, &d_y_m, &d_xx_m, &d_yy_m, &d_xy_m); // at first seeable speedup, then progressively worse overhead with more threads
 
             double delta_y = get_delta_m_y(d_x_m, d_y_m, d_xx_m, d_yy_m, d_xy_m);
             double delta_x = get_delta_m_x(d_x_m, d_y_m, d_xx_m, d_yy_m, d_xy_m, delta_y);
             kk->coords[delta_max_index].x += delta_x;
             kk->coords[delta_max_index].y += delta_y;
 
-            kk->deltas[delta_max_index] = get_delta_m_seq(kk, delta_max_index); // at first seeable speedup, then progressively worse overhead with more threads
+            kk->deltas[delta_max_index] = get_delta_m_par(kk, delta_max_index); // at first seeable speedup, then progressively worse overhead with more threads
             if (steps-- <= 0)
                 break;
         }
 
-        int temp = update_deltas_seq(kk, delta_max_index);
+        int temp = update_deltas_par(kk, delta_max_index);
         if (temp == delta_max_index)
             break;
         delta_max_index = temp;
